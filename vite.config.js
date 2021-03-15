3a5c84e93a7107d1bd55ab6d750a104259e60710
Stanley <stanleyyylau@gmail.com>
2021-03-15 18:09:23 +0800

    delete viete

--- vite.config.js
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from 'vite'
-// import vue from '@vitejs/plugin-vue'
-const { createVuePlugin } = require('vite-plugin-vue2')
-
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [createVuePlugin()]
-})
